21defb0b79a6a3e0f9f7dabde7f4e4c10f0cc9ad
ColtonWaldack <76004887+ColtonWaldack@users.noreply.github.com>
2021-04-14 23:50:00 -0400

    Update BST.java

--- BST.java
+++ BST.java
@@ -1,59 +1,57 @@
-class BST {
-	private Node root; 
-    //Basic Node definition. Each Node contains a Value, a left child, and a right child
-    class Node { 
-        int val; 
-        Node left, right; 
-  
-        public Node(int value) { 
-        	val = value; 
-            left =null;
-            right = null; 
-        } 
-    } 
-  
-  
-    public BST(int rootValue) {  
-        root = new Node(rootValue);  
-    } 
-  
-    //insert Value into the appropriate spot in the tree
-    private Node insert(Node root,int value) { 
-  
+import javax.swing.*;
 
-        if (root == null) { 
-            root = new Node(value); 
-            return root; 
-        } 
-  
-        if (value < root.val) 
-            root.left = insert(root.left, value); 
-        else if (value > root.val) 
-            root.right = insert(root.right, value); 
-  
-        return root; 
-    } 
-  
- 
-    //finish code to print all values with an inorder traversal
+class BST {
+    private Node root;
+    private boolean check;
+    class Node {
+        int val;
+        Node left, right;
+
+        public Node(int value) {
+            val = value;
+            left = null;
+            right = null;
+        }
+    }
+
+
+    public BST(int rootValue) {
+        root = new Node(rootValue);
+    }
+    
+    private Node insert(Node root, int value) {
+
+
+        if (root == null) {
+            root = new Node(value);
+            return root;
+        }
+
+        if (value < root.val)
+            root.left = insert(root.left, value);
+        else if (value > root.val)
+            root.right = insert(root.right, value);
+
+        return root;
+    }
+
     void inorderTraversal(Node root) {
         if (root == null)
             return;
         inorderTraversal(root.left);
         System.out.print(root.val + " ");
         inorderTraversal(root.right);
-        }
+    }
 
-    void preorderTraversal(Node root)
-    {
+    void preorderTraversal(Node root) {
         if (root == null)
             return;
         System.out.print(root.val + " ");
         preorderTraversal(root.left);
         preorderTraversal(root.right);
     }
-    void postorderTraversal(Node root)
-    {
+
+    void postorderTraversal(Node root) {
         if (root == null)
             return;
         postorderTraversal(root.left);
@@ -61,22 +59,59 @@ class BST {
         System.out.print(root.val + " ");
     }
 
-  
-    public static void main(String[] args) { 
-    	//create tree with a root of 5
-        BST tree = new BST(5);   
-      
-        tree.insert(tree.root,2); 
-        tree.insert(tree.root,7); 
-        tree.insert(tree.root,10);
-        tree.insert(tree.root,4); 
-        tree.insert(tree.root,1);
+    private boolean search(Node root, int n) {
+
+    if (root == null) return false;
+    if (root.val == n) return true;
+    if (root.val > n) return search(root.left, n);
+    return search(root.right, n);
+}
+
+    public static void main(String[] args) {
+        //create tree with a root of 5
+        BST tree = new BST(5);
+        boolean check = false;
+
+        tree.insert(tree.root, 2);
+        tree.insert(tree.root, 7);
+        tree.insert(tree.root, 10);
+        tree.insert(tree.root, 4);
+        tree.insert(tree.root, 1);
 
         //start traversal at root
-       System.out.println("Inorder Traversal: " ); tree.inorderTraversal(tree.root);
-       System.out.println("");
-       System.out.println("Preorder Traversal: " ); tree.preorderTraversal(tree.root);
-       System.out.println("");
-       System.out.println("Postorder Traversal: " ); tree.postorderTraversal(tree.root);
-    } 
+        System.out.println("Inorder Traversal: ");
+        tree.inorderTraversal(tree.root);
+        System.out.println("");
+        System.out.println("Preorder Traversal: ");
+        tree.preorderTraversal(tree.root);
+        System.out.println("");
+        System.out.println("Postorder Traversal: ");
+        tree.postorderTraversal(tree.root);
+
+        int reply = JOptionPane.showConfirmDialog(null, "Would you like to Search for a specific number?", "Number Search?", JOptionPane.YES_NO_OPTION);
+        if (reply == JOptionPane.YES_OPTION) {
+            String n = JOptionPane.showInputDialog("Please enter a number");
+             if (tree.search(tree.root, Integer.parseInt(n))){
+
+                  check = true;
+
+
+                 JOptionPane.showMessageDialog(null,( Integer.parseInt(n) + " Was found in the tree."));
+             }
+            if (!check){
+
+                 JOptionPane.showMessageDialog(null,( Integer.parseInt(n) + " Was not found in the tree."));
+            }
+
+             }
+
+
+         else {
+            JOptionPane.showMessageDialog(null, "Thanks for using my program.");
+            System.exit(0);
+        }
+
+
+    }
+
 }
